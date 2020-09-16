b0457c1b3f5eb7c5db531d224c271619274f252d
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-09-16 16:52:31 -0500

    using basic login right now, want to simplify if I have time

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,66 +1,64 @@
-import React, {Component, useState} from 'react';
+import React, {Component} from 'react';
 import NavBar from './components/NavBar';
+import Listings from './components/Listings';
 import { Button, TextField , Card, CardContent } from '@material-ui/core';
 import './App.css';
-import Listings from './components/Listings'
-
-//need to pass in the status from the original state
 
 class LogIn extends Component {
-  state = {
-    logInStatus: false,
-    username: '',
-    password:''
+  constructor(props) {
+    super(props)
+    this.state = {
+      loggedIn: false,
+      username: '',
+      password: ''
+   };
+  };
+
+  clickHandler = (e) => {
+   // e.preventDefault();
+    //console.log(this.state)
+
+    if (this.state.username === '') {
+      alert(`You haven't entered a username yet`) 
+    } else if (this.state.password === '') {
+      alert(`You must have a password`)
+    } else if (this.state.username !== '' && this.state.password !== '') {
+    this.setState({
+      loggedIn: !this.state.loggedIn,
+    })
+  }
+    console.log(this.state)
+  };
+
+  handleName = e => {
+      this.setState({
+        username: e.target.value,
+      })
   }
 
-  handleText = (e) => {
-    const newState = {...this.state}
-    newState[e.target.id] = e.target.value
-    this.setState(newState)
+  handlePassword = e => {
+    this.setState({
+      password: e.target.value
+    })
   }
-    // e.preventDefault();
-     //console.log(this.state)
- 
-  //    if (this.username === '') {
-  //      alert(`You haven't entered a username yet`) 
-  //    } else if (this.password === '') {
-  //      alert(`You must have a password`)
-  //    } else if (username !== '' && password !== '') {
-  //    this.setState({
-  //      loggedIn: !this.state.loggedIn,
-  //    })
-  //  }
-  //    console.log(this.state)
-  //  };
- 
-  //  handleName = e => {
-  //      this.setState({
-  //        username: e.target.value,
-  //      })
-  //  }
- 
-  //  handlePassword = e => {
-  //    this.setState({
-  //      password: e.target.value
-  //    })
-  //  }
+
+  render() { 
   
-   render() {
-    return loggedIn ? <Listings user={username}/> : (
+    return this.state.loggedIn ? <Listings user={this.state.username}/> : (
       <div>
-        <NavBar/>
+        <NavBar />
           <Card className="logInCard">
             <CardContent>
-               <TextField label="Username *" />
+               <TextField onChange={this.handleName} label="Username *" />
                <br />
-               <TextField type="password" id="outlined-password-input" label="Password *" />
+               <TextField onChange={this.handlePassword} type="password" id="outlined-password-input" label="Password *" />
                <br />
-               <Button className="logInButton">Log In</Button>
+               <Button className="logInButton" onClick={this.clickHandler}>Log In</Button>
             </CardContent>
           </Card>
       </div>
     )
-   }
-   }
+  }
+}
 
 export default LogIn;
