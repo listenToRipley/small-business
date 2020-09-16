99b629c28951e7e20fc60fd841ae793188997066
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-09-15 20:52:41 -0500

    update reducer

---
+++ src/redux/reducer.js
@@ -0,0 +1,16 @@
+import { combineReducers} from 'redux'
+
+const logIn = (state = false) => state
+
+const businesses = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_BUSINESS':
+      return [...state, action.value]
+    case 'DELETE_BUSINESS':
+      const businesses = [...state]
+      businesses.splice(action.value, 1)
+      return businesses
+  }
+}
+
+export default combineReducers({logIn, business})
