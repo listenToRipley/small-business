0d10e9c695df8fada96d01c1ab5db6e8e592edbc
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-09-19 13:56:46 -0500

    had to modify the params input for business, now working!

--- src/components/Business.js
+++ src/components/Business.js
@@ -2,18 +2,20 @@ import React from 'react';
 import {Container, Paper} from '@material-ui/core';
 
 const Business = (props) => {
+ 
     console.log('business props :', props)
-  const id = props.business.params.id
-    console.log('it is : ', typeof props.id, ' and it is :', props.id)
+    const id = props.match.params.id.substring(1)
+
+    console.log('it is : ', typeof id, ' and it is :', id)
   const business = props.businesses.find(b => b.id === id)
 
   return (
     <Container maxWidth="sm" className="business-container">
         <Paper className="business-paper">
-            <h2>Business Name : {business.name}</h2>
-            <h2>Address{business.address}</h2>
-            <h2>Hours {business.hours}</h2>
-            <h2>Description{business.description}</h2>
+            <h2>{business.name}</h2>
+            <h4>{business.address}</h4>
+            <h4>{business.hours}</h4>
+            <p>{business.description}</p>
         </Paper>
     </Container>
 )
