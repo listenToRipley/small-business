ca5116b3a48c6adb962b62c5900bd1d86ff11c74
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-09-20 20:26:24 -0500

    well something went very strange

--- src/components/Business.js
+++ src/components/Business.js
@@ -4,22 +4,21 @@ import './componentcss.css'
 import CookieReader from './CookieReader';
 
 const Business = (props) => {
-    const businessArray = props.businesses
-    console.log('business props', businessArray)
-  const businessId = props.match.params.id.substring(1)
-  console.log('id is: ', businessId)
-  const business = businessArray.find(b => b.id === businessId)
-  console.log('THE BUSINESS IS', business)
+    console.log('business props',props.businesses)
+    const businessId = props.match.params.id.substring(1)
+    console.log('id is: ', businessId)
+    const business = props.businesses.find( id => id === businessId)
+    console.log('THE BUSINESS IS', business)
 
   return (
     <Fragment>
         <CookieReader/>
         <Container maxWidth="sm" className="businessContainer">
             <Paper className="businessPaper">
-                <h2 className="businessInfoItem">{business.name}</h2>
-                <h4 className="businessInfoItem">{business.address}</h4>
-                <h4 className="businessInfoItem">{business.hours}</h4>
-                <p className="businessInfoItem">{business.description}</p>
+                <h2 className="businessInfoItem">{name}</h2>
+                <h4 className="businessInfoItem">{address}</h4>
+                <h4 className="businessInfoItem">{hours}</h4>
+                <p className="businessInfoItem">{description}</p>
                 <div className="map">   MAP</div>
             </Paper>
         </Container>
